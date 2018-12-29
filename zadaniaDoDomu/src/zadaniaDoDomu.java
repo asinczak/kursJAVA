60e2b977bb71c158db1462e9ea73cfa81a85f7ad
asinczak <agatasinczak@wp.pl>
2018-12-29 22:53:40 +0100

    poprawione 1 zadanie

--- zadaniaDoDomu/src/zadaniaDoDomu.java
+++ zadaniaDoDomu/src/zadaniaDoDomu.java
@@ -4,24 +4,25 @@ public class zadaniaDoDomu {
         // Napisz program który dla podanej liczby całkowitej wyświetla jej dzielniki.
         // Przykład dla np. liczby 21 dzielniki to: 1, 3, 7, 21
 
-        int liczbaCalkowita = 21;
+        int liczbaCalkowita = 50;
         int dzielnik = 0;
-        int i = 1;
+        int i;
 
 
-        while (i <= liczbaCalkowita) {
 
-            if (liczbaCalkowita % i == 0) ;
-            System.out.println("Dzielniki " + liczbaCalkowita + " to: " + i);
+        for (i = 1; i <= liczbaCalkowita; i++) {
 
 
-            dzielnik++;
-            i++;
+            if (liczbaCalkowita % i == 0) {
+
+
+                System.out.println("Dzielniki " + liczbaCalkowita + " to: " + i);
+
+
+            }
+
 
         }
-
-
-
 
 
 
