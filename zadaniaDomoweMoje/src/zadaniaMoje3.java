d6e206b1995585b34e9ca6cea58aad58127439b4
asinczak <agatasinczak@wp.pl>
2019-01-05 19:29:23 +0100

    zadania moje

---
+++ zadaniaDomoweMoje/src/zadaniaMoje3.java
@@ -0,0 +1,19 @@
+public class zadaniaMoje3 {
+    public static void main(String[] args) {
+
+        // Napisać program, który wczytuje liczbę całkowitą dodatnią n, a
+        //następnie wyświetla na ekranie wszystkie potęgi liczby 2 nie większe, niż podana liczba.
+
+        int liczbaCalkowita = 71;
+        int wynik = 1;
+
+
+        while ( wynik < liczbaCalkowita) {
+
+            System.out.println(wynik);
+            wynik = wynik * 2;
+
+        }
+
+    }
+}
