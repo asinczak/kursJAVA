c5096bf1fd254dcd0cb2c084a0cd78274651f313
asinczak <agatasinczak@wp.pl>
2019-01-06 18:45:25 +0100

    zadania moje

---
+++ zadaniaDomoweMoje/src/zadaniaMoje6.java
@@ -0,0 +1,25 @@
+public class zadaniaMoje6 {
+    public static void main(String[] args) {
+
+        //Napisz program który wyświetli że program zrobił 23 pompki
+
+        int i;
+
+        for (i = 0; i < 23; i++) {
+
+        }
+        System.out.println("Program zrobił " + i + " pompki") ;
+
+
+
+        // Napisz program który wyświetli wszystkie wartości w tablicy typu String
+
+
+        String drzewaIglaste [] = {"sosna", "jodła", "modrzew", "świerk", "jałowiec"};
+
+        for (String drzewa : drzewaIglaste) {
+            System.out.println( drzewa );
+        }
+
+    }
+}
