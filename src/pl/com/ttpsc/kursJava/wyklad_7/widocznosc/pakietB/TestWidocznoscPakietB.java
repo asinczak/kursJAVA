8095bfceae71e89384d3084610eccd3abb3f3e6a
amorpiwo <amorpiwo@gmail.com>
2019-01-29 11:25:01 +0100

    Dodatkowe komentarze

--- src/pl/com/ttpsc/kursJava/wyklad_7/widocznosc/pakietB/TestWidocznoscPakietB.java
+++ src/pl/com/ttpsc/kursJava/wyklad_7/widocznosc/pakietB/TestWidocznoscPakietB.java
@@ -6,7 +6,8 @@ public class TestWidocznoscPakietB {
 
     public static void main(String[] args) {
         Pochodna p = new Pochodna();
-        //dostep do zmiennej o widocznosci protekted poza pakietem i poza klasa dziedziczaca
+        //dostep do zmiennej o widocznosci protected poza pakietem i poza klasa dziedziczaca
+        //nie da sie w ten sposob dobrac do takiej zmiennej
 
         p.pakietowa = 0;
 
