e6ae6eaf006a3f763de99706c42d22b89e3a0e0b
asinczak <agatasinczak@wp.pl>
2019-02-14 09:40:49 +0100

    changes

---
+++ src/pl/com/ttpsc/kursJava/Piotrek/wyklad_7/interfaces/Zwierze.java
@@ -0,0 +1,7 @@
+package pl.com.ttpsc.kursJava.Piotrek.wyklad_7.interfaces;
+
+public interface Zwierze {
+
+    void jedz();
+
+}
---
+++ src/pl/com/ttpsc/kursJava/Piotrek/wyklad_7/widocznosc/pakietA/Baza.java
@@ -0,0 +1,7 @@
+package pl.com.ttpsc.kursJava.Piotrek.wyklad_7.widocznosc.pakietA;
+
+public class Baza {
+
+    protected int pakietowa;
+
+}
