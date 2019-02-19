e24f6a5ad55e292ac0c8fc494bab994d36bbb0f2
amorpiwo <amorpiwo@gmail.com>
2019-02-19 13:58:01 +0100

    Wyklad 9 - klasy opakowujace

---
+++ src/pl/com/ttpsc/kursJava/wyklad_9/Wrappers.java
@@ -0,0 +1,26 @@
+package pl.com.ttpsc.kursJava.wyklad_9;
+
+public class Wrappers {
+
+    public static void main(String[] args) {
+
+        //automatycznie wartosc 10001 (typ int) jest opakowana w obiekt klasy Integer
+        Integer integer = 10001;
+
+        //z obiektu opakowujacego wyciagana jest automatycznie wartosc prosta (typ int)
+        int i = integer;
+
+        byte by = integer.byteValue();
+
+        System.out.println(by);
+
+        double d = add(3.0, 9);
+
+    }
+
+    static Double add(Double d1, Integer i1) {
+        return d1 + i1;
+    }
+
+
+}
