88c455c6ba1811edabbaab1024561da1589e3537
amorpiwo <amorpiwo@gmail.com>
2019-02-19 16:37:56 +0100

    Propozycja definiowania stalych na projekcie

---
+++ src/pl/com/ttpsc/kursJava/constants/Stale.java
@@ -0,0 +1,22 @@
+package pl.com.ttpsc.kursJava.constants;
+
+public class Stale {
+
+    private Stale() {}
+
+
+    public static class Math {
+
+        public static final double PI = 3.14;
+        public static final double E = 3.14;
+        public static final double DELTA = 3.14;
+        public static final double SIGMA = 3.14;
+
+    }
+    public static class Time {
+
+        public static final double SEKUN_NA_GODZINE = 3600;
+
+    }
+
+}
