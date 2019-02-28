e8a8d51f74505ff4aeb9bfe5c434877d9c1acb50
amorpiwo <amorpiwo@gmail.com>
2019-02-28 15:58:58 +0100

    Maly update do generykow

--- src/pl/com/ttpsc/kursJava/Piotrek/generics/Gen.java
+++ src/pl/com/ttpsc/kursJava/Piotrek/generics/Gen.java
@@ -20,10 +20,16 @@ public class Gen<Z> {
         Gen<Integer> g2 = new Gen<>(1);
         Gen<CurrentAccount> g3 = new Gen<>(new CurrentAccount(1));
 
+        Gen g4 = new Gen<>(new CurrentAccount(1));
+
+
         String obj = g1.getObj();
 
         CurrentAccount obj1 = g3.getObj();
         obj1.balanceDecrease(12);
+
+        //nie ma typy podanego przy deklaracji, jedyne co mozna powiedziec to ze na pewno jest to Object
+        Object obj2 = g4.getObj();
 
     }
 }
